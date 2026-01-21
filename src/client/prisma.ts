0e0d62d53fba2832f1eb0e930872e91269a1f7ca
rstubryan <restubumirr@gmail.com>
2026-01-21 22:05:20 +0700

    refactor(conn): Use Prisma Postgres adapter with pg Pool

--- src/client/prisma.ts
+++ src/client/prisma.ts
@@ -1,6 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import * as pg from 'pg'
+
+const { Pool } = pg
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+})
+
+const adapter = new PrismaPg(pool)
 
 const prisma = new PrismaClient({
+  adapter,
   log: ['info', 'error', 'query', 'warn'],
   errorFormat: 'pretty',
 })
