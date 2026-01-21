0d642f31b3c8a1706599773eab01c683e0fb7431
rstubryan <restubumirr@gmail.com>
2026-01-21 22:35:38 +0700

    feat(auth): Set auth tokens as cookies on login/register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,21 +1,48 @@
 import { Hono } from 'hono'
-import { UserService } from '../services/users'
+import { setCookie } from 'hono/cookie'
+import { UserService } from '../services/users.service'
 import { AuthRegisterRequest, AuthLoginRequest } from '../types/api/auth/auth'
+import {
+  COOKIE_NAMES,
+  COOKIE_OPTIONS,
+  ACCESS_TOKEN_MAX_AGE,
+  REFRESH_TOKEN_MAX_AGE,
+} from '../utils/cookie'
 
 export const authController = new Hono()
 
 authController.post('/register', async (c) => {
   const request = (await c.req.json()) as AuthRegisterRequest
 
-  const response = await UserService.register(request)
+  const { user, tokens } = await UserService.register(request)
 
-  return c.json(response)
+  setCookie(c, COOKIE_NAMES.ACCESS_TOKEN, tokens.accessToken, {
+    ...COOKIE_OPTIONS,
+    maxAge: ACCESS_TOKEN_MAX_AGE,
+  })
+
+  setCookie(c, COOKIE_NAMES.REFRESH_TOKEN, tokens.refreshToken, {
+    ...COOKIE_OPTIONS,
+    maxAge: REFRESH_TOKEN_MAX_AGE,
+  })
+
+  return c.json(user)
 })
 
 authController.post('/login', async (c) => {
   const request = (await c.req.json()) as AuthLoginRequest
 
-  const response = await UserService.login(request)
+  const { user, tokens } = await UserService.login(request)
 
-  return c.json(response)
+  setCookie(c, COOKIE_NAMES.ACCESS_TOKEN, tokens.accessToken, {
+    ...COOKIE_OPTIONS,
+    maxAge: ACCESS_TOKEN_MAX_AGE,
+  })
+
+  setCookie(c, COOKIE_NAMES.REFRESH_TOKEN, tokens.refreshToken, {
+    ...COOKIE_OPTIONS,
+    maxAge: REFRESH_TOKEN_MAX_AGE,
+  })
+
+  return c.json(user)
 })
